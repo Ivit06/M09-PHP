ba2041aebcb9b1225d3c2068203527d347c4d2d5
Asix1 <asix1@Portatil.asix1.org>
2024-05-04 03:28:12 +0200

    Actividad 2 finalizda

--- Actividad 2/funciones.php
+++ Actividad 2/funciones.php
@@ -27,37 +27,128 @@ function connectDatabase() {
 }
 
 function confi_getopt(){
-	$a = getopt('o:i::t::d::', ['option:', 'id::', 'tarea::', 'descripción::']);
+	$a = getopt('o:i:t:d:', ['option:', 'id:', 'titulo:', 'descripción:']); #He tenido que poner todo con solo : porque si no me obligaba a poner el valor a continuación del parámetro (-iHola)
 	return $a;
 }
 
-function comprovar_acc_get($options) {
-	if (isset($options['o']) && isset($options['option'])) { #comprovamos que el usuario no nos pase los dos parámetro para la acción. Si lo hace le damos la ayuda
+function comprobar_acc_get($options) {
+	if (isset($options['o']) && isset($options['option'])) { #comprobamos que el usuario no nos pase los dos parámetro para la acción. Si lo hace le damos la ayuda
 		return more_than_one(); 
 	}
 	if (isset($options['o'])) {
 		$value = $options['o'];
-		return $value; #Queremos que retorne el valor para poder usarlo en el swich
+		return $value;
 	}
 	if (isset($options['option'])) {
 		$value = $options['option'];
-		return $value; #Queremos que retorne el valor para poder usarlo en el swich
+		return $value;
 	}
 	else{
 		return help();
 	}
 }
 
-function add_to_bd($argv) {	#ahora en estas funciones tendré que extraer los valores que me interesen con una función parecida a comprovar_acc_get y pasarlos como valor sustituyendo $argv
+function comprobar_id_get($options) {
+	if (isset($options['i']) && isset($options['id'])) {
+		return more_than_one(); 
+	}
+	if (isset($options['i'])) {
+		$value = $options['i'];
+		return $value;
+	}
+	if (isset($options['id'])) {
+		$value = $options['id'];
+		return $value;
+	}
+	else{
+		return help();
+	}
+}
+
+function comprobar_titulo_get($options) {
+	if (isset($options['t']) && isset($options['titulo'])) {
+		return more_than_one(); 
+	}
+	if (isset($options['t'])) {
+		$value = $options['t'];
+		return $value;
+	}
+	if (isset($options['titulo'])) {
+		$value = $options['titulo'];
+		return $value;
+	}
+	else{
+		return help();
+	}
+}
+
+function comprobar_desc_get($options) {
+	if (isset($options['d']) && isset($options['descripción'])) {
+		return more_than_one(); 
+	}
+	if (isset($options['d'])) {
+		$value = $options['d'];
+		return $value;
+	}
+	if (isset($options['descripción'])) {
+		$value = $options['descripción'];
+		return $value;
+	}
+	else{
+		return help();
+	}
+}
+
+function add_to_bd($options) {	
 	$servername = "localhost";
     $username = "task_manager";
     $password = "mysql12345";
     $dbname = "task";
     $con = new mysqli($servername, $username, $password, $dbname);
-	$titulo = $argv[2];
-	$descripción = $argv[3];
+	$titulo = comprobar_titulo_get($options);		#Las funciones las llamo aquí por si hay error para que me retornen el help.
+	$descripción = comprobar_desc_get($options);
 	mysqli_query($con, "INSERT INTO task.Todas (Titulo, Descripción, Estado) VALUES ('$titulo', '$descripción', 'incompleto')");
-	echo "Tarea añadida.\n";
+	$m = tarea_a();
+	echo $m;
+	$con->close();
+}
+
+function read_to_bd() {
+	$servername = "localhost";
+	$username = "task_manager";
+	$password = "mysql12345";
+	$dbname = "task";
+	$con = new mysqli($servername, $username, $password, $dbname);
+	$result = mysqli_query($con,"SELECT * FROM task.Todas");
+	while ($row = $result->fetch_assoc()) {   
+		echo $row['ID'] , ' --- ' , $row['Titulo'] , ' --- ' , $row['Descripción'] ,' --- ', $row['Estado'], "\n";
+	}
+	$con->close();
+}
+
+function complete_to_bd($options) { 
+	$servername = "localhost";
+	$username = "task_manager";
+	$password = "mysql12345";
+	$dbname = "task";
+	$con = new mysqli($servername, $username, $password, $dbname);
+	$id = comprobar_id_get($options);
+	mysqli_query($con,"UPDATE task.Todas SET Estado='complete' WHERE ID = $id");
+	$m = tarea_c();
+	echo $m;
+	$con->close();
+}
+
+function delete_to_bd($options) {
+	$servername = "localhost";
+	$username = "task_manager";
+	$password = "mysql12345";
+	$dbname = "task";
+	$con = new mysqli($servername, $username, $password, $dbname);
+    $id = comprobar_id_get($options);
+	mysqli_query($con,"DELETE FROM task.Todas WHERE ID = $id");
+	$m = tarea_e();
+	echo $m;
 	$con->close();
 }
 
--- Actividad 2/mensajes.php
+++ Actividad 2/mensajes.php
@@ -2,22 +2,35 @@
 
 function help() {
     echo "Error: No s'han proporcionat suficients arguments.\n";
-    echo "Uso: php task_manager.php [opción]\n";
+    echo "Uso: php task_manager_get.php (-o or --option)\n";
     echo "Opciones:\n";
-    echo "  add 'tarea' 'descripción'	Añade una tarea\n";
-    echo "  show				Muestra todas las tareas\n";
-    echo "  complete id			Marca una tarea como completada\n";
-    echo "  delete id			Elimina una tarea\n";
-    echo "  help				Muestra este mensaje de ayuda\n";
+    echo " add (-t or --titulo) 'titulo' (-d or --descripción) 'descripción'    Añade una tarea\n";
+    echo " show                                                                 Muestra todas las tareas\n";
+    echo " complete (-i or --id) id.tarea                                       Marca una tarea como completada\n";
+    echo " delete (-i or --id) id.tarea                                         Elimina una tarea\n";
+    echo " help                                                                 Muestra este mensaje de ayuda\n";
     }
 
 function more_than_one(){
-    return "Estas utilizando más de un parámetro: \nEjemplo: Para determinar acción utilize -o o --option (NUNACA LAS DOS A LA VEZ)\n";
+    return "Estas utilizando más de un parámetro: \nEjemplo: Para determinar acción utilize -o or --option (NUNACA LAS DOS A LA VEZ)\n";
 }
 
 function aviso_cli() {
-    return "Aquest script només es pot executar des del CLI.\n";
+    return "Este script solo se puede ejecutar en cli.\n";
 }
+
+function tarea_a(){
+    return "Tarea añadida.\n";
+}
+
+function tarea_c(){
+    return "Tarea completada.\n";
+}
+
+function tarea_e(){
+    return "Tarea eliminada.\n";
+}
+
 ?>
 
 
